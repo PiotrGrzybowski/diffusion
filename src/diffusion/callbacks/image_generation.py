93926a8021f89a34123b913ceb282a6e75b22d62
Piotr Grzybowski <p.grzybowski2@gmail.com>
2024-11-08 10:51:23 +0100

    feat: add image generation callback parametrization

--- src/diffusion/callbacks/image_generation.py
+++ src/diffusion/callbacks/image_generation.py
@@ -1,22 +1,25 @@
 import math
 from pathlib import Path
 
-from lightning import Callback
+import torch
+from lightning import Callback, LightningModule, Trainer
 from lightning.fabric.utilities.rank_zero import rank_zero_only
 from torchvision.utils import save_image
 
 
 class ImageGenerationCallback(Callback):
-    def __init__(self, output_dir: str):
+    def __init__(self, shape: list[int], output_dir: Path | str, every_n_epochs: int = 1) -> None:
         super().__init__()
+        self.shape = list(shape)
         self.output_dir = Path(output_dir)
+        self.every_n_epochs = every_n_epochs
 
     @rank_zero_only
-    def on_validation_epoch_end(self, trainer, pl_module):
-        if (trainer.current_epoch + 1) % 10 == 0:
-            dataloader = trainer.datamodule.predict_dataloader()
+    def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
+        if (trainer.current_epoch + 1) % self.every_n_epochs == 0:
             pl_module.eval()
-            result = pl_module.predict_step(next(iter(dataloader)).to(pl_module.device))
+            x = torch.randn(self.shape, device=pl_module.device)
+            result = pl_module(x)
             path = self.output_dir / "images"
             path.mkdir(exist_ok=True)
             save_image(result, path / f"sample_{trainer.current_epoch}.png", nrow=int(math.sqrt(result.shape[0])))
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,15 +1,13 @@
+from pathlib import Path
+
 from diffusion.train import train
 from hydra import compose, initialize_config_dir
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, open_dict
-from rootutils import rootutils
 
 
-configs_path = rootutils.find_root("pyproject.toml") / "configs"
-
-
-def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
-    with initialize_config_dir(config_dir=str(configs_path)):
+def test_train_fast_dev_run(cfg_train: DictConfig, configs_path: Path) -> None:
+    with initialize_config_dir(config_dir=str(configs_path), version_base="1.3"):
         model_config = compose(config_name="model/little")
 
         with open_dict(cfg_train):
@@ -19,5 +17,7 @@ def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
     with open_dict(cfg_train):
         cfg_train.trainer.fast_dev_run = True
         cfg_train.trainer.accelerator = "cpu"
+        cfg_train.logger = None
+        cfg_train.callbacks = None
         cfg_train.test = False
     train(cfg_train)
