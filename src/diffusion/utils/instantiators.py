09ef63d1dd6966eb05ed5fe99fbd099b07b6df1d
Piotr Grzybowski <p.grzybowski2@gmail.com>
2025-02-09 18:33:03 +0100

    feat: add wandb image upload and hyperparameter logging

--- src/diffusion/utils/instantiators.py
+++ src/diffusion/utils/instantiators.py
@@ -9,43 +9,27 @@ from diffusion.utils.ranked_logger import RankedLogger
 log = RankedLogger(__name__, rank_zero_only=True)
 
 
-def instantiate_callbacks(callbacks_cfg: DictConfig) -> list[Callback]:
-    """Instantiates callbacks from config.
-
-    :param callbacks_cfg: A DictConfig object containing callback configurations.
-    :return: A list of instantiated callbacks.
-    """
+def instantiate_callbacks(callbacks_config: DictConfig) -> list[Callback]:
     callbacks: list[Callback] = []
 
-    if not callbacks_cfg:
+    if not callbacks_config:
         log.warning("No callback configs found! Skipping..")
         return callbacks
 
-    if not isinstance(callbacks_cfg, DictConfig):
-        raise TypeError("Callbacks config must be a DictConfig!")
-
-    for _, cb_conf in callbacks_cfg.items():
-        if isinstance(cb_conf, DictConfig) and "_target_" in cb_conf:
-            log.info(f"Instantiating callback <{cb_conf._target_}>")
-            callbacks.append(hydra.utils.instantiate(cb_conf))
+    for _, config in callbacks_config.items():
+        if isinstance(config, DictConfig) and "_target_" in config:
+            log.info(f"Instantiating callback <{config._target_}>")
+            callbacks.append(hydra.utils.instantiate(config))
 
     return callbacks
 
 
 def instantiate_loggers(logger_cfg: DictConfig) -> list[Logger]:
-    """Instantiates loggers from config.
-
-    :param logger_cfg: A DictConfig object containing logger configurations.
-    :return: A list of instantiated loggers.
-    """
     logger: list[Logger] = []
 
     if not logger_cfg:
         log.warning("No logger configs found! Skipping...")
         return logger
-
-    if not isinstance(logger_cfg, DictConfig):
-        raise TypeError("Logger config must be a DictConfig!")
 
     for _, lg_conf in logger_cfg.items():
         if isinstance(lg_conf, DictConfig) and "_target_" in lg_conf:
