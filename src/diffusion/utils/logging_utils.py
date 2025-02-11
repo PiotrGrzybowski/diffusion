bedd0ebb3f58f8e69d53d2e0a1f63b3cfb8dfc1b
Piotr Grzybowski <p.grzybowski2@gmail.com>
2025-02-11 18:04:53 +0100

    Add hparam logging

--- src/diffusion/utils/logging_utils.py
+++ src/diffusion/utils/logging_utils.py
@@ -24,30 +24,35 @@ def log_hyperparameters(object_dict: dict[str, Any]) -> None:
     hparams = {}
 
     cfg = OmegaConf.to_container(object_dict["cfg"])
-    model = object_dict["model"]
+    # model = object_dict["model"]
+    diffusion = object_dict["diffusion"]
     trainer = object_dict["trainer"]
 
     if not trainer.logger:
         log.warning("Logger not found! Skipping hyperparameter logging...")
         return
 
-    hparams["model"] = cfg["model"]
+    # hparams["model"] = cfg["model"]
+    hparams["diffusion"] = diffusion
 
     # save number of model parameters
-    hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
-    hparams["model/params/trainable"] = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    hparams["model/params/non_trainable"] = sum(p.numel() for p in model.parameters() if not p.requires_grad)
+    # hparams["model/params/total"] = sum(p.numel() for p in model.parameters())
+    # hparams["model/params/trainable"] = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # hparams["model/params/non_trainable"] = sum(p.numel() for p in model.parameters() if not p.requires_grad)
 
-    hparams["data"] = cfg["data"]
-    hparams["trainer"] = cfg["trainer"]
-
-    hparams["callbacks"] = cfg.get("callbacks")
-    hparams["extras"] = cfg.get("extras")
-
-    hparams["task_name"] = cfg.get("task_name")
-    hparams["tags"] = cfg.get("tags")
-    hparams["ckpt_path"] = cfg.get("ckpt_path")
-    hparams["seed"] = cfg.get("seed")
+    # hparams["data"] = cfg["data"]
+    # hparams["trainer"] = cfg["trainer"]
+    #
+    # hparams["callbacks"] = cfg.get("callbacks")
+    # hparams["extras"] = cfg.get("extras")
+    #
+    # hparams["task_name"] = cfg.get("task_name")
+    # hparams["tags"] = cfg.get("tags")
+    # hparams["ckpt_path"] = cfg.get("ckpt_path")
+    # hparams["seed"] = cfg.get("seed")
+    hparams["tags"] = ["vlb", "fixed_small", "epsilon_mean"]
+    hparams["loss"] = "vlb"
+    hparams["mean"] = "epsilon"
 
     # send hparams to all loggers
     for logger in trainer.loggers:
