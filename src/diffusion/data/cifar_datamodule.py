feat: update CIFAR-10 datamodule and fix VLB loss numerical stability

src/diffusion/data/cifar_datamodule.py
@@ -1,11 +1,15 @@
 import ssl
 from pathlib import Path
+from tempfile import gettempdir
 
 import torch
 from lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import transforms
+
+from diffusion.data.dataset_map import DatasetMap
+from diffusion.data.filtered_mnist import FilteredDataset
 
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -26,18 +30,20 @@ class GaussianDataset(Dataset):
 class CIFAR10DataModule(LightningDataModule):
     def __init__(
         self,
-        path: Path = Path("/tmp/data"),
+        dataset_name: str = "cifar10",
+        path: Path = Path(gettempdir()),
         val_split: float = 0.95,
         batch_size: int = 64,
         num_workers: int = 0,
         pin_memory: bool = False,
-        predict_size: int = 16,
+        predict_samples: int = 16,
     ) -> None:
         super().__init__()
 
         self.path = path
         self.batch_size = batch_size
-        self.predict_size = predict_size
+        self.dataset_class = DatasetMap.get_dataset_class(dataset_name.lower())
+        self.predict_samples = predict_samples
         self.val_split = val_split
         self.num_workers = num_workers
         self.pin_memory = pin_memory
@@ -60,15 +66,11 @@ class CIFAR10DataModule(LightningDataModule):
     def setup(self, stage: str | None = None) -> None:
         self._check_batch_size_compatibility()
         if not self._is_setup():
-            generator = torch.Generator().manual_seed(42)
-            train_set, test_set = self._load()
-            train_length, val_length = self._train_val_lengths(train_set, self.val_split)
-            train_set, val_set = random_split(train_set, lengths=[train_length, val_length], generator=generator)
+            train_dataset = FilteredDataset(self.dataset_class(self.path, train=True, transform=self.transforms), None, None)
+            test_dataset = FilteredDataset(self.dataset_class(self.path, train=False, transform=self.transforms), None, 8)
 
-            self.data_train = train_set
-            self.data_val = val_set
-            self.data_test = test_set
-            self.data_predict = GaussianDataset((self.predict_size, 3, 32, 32))
+            self.data_train = train_dataset
+            self.data_val = test_dataset
 
     def train_dataloader(self) -> DataLoader[tuple[torch.Tensor, torch.Tensor]]:
         if self.data_train:
@@ -94,19 +96,19 @@ class CIFAR10DataModule(LightningDataModule):
         else:
             raise RuntimeError("The validation dataset is not loaded.")
 
-    def test_dataloader(self) -> DataLoader[tuple[torch.Tensor, torch.Tensor]]:
-        self.data_predict = GaussianDataset((self.predict_size, 1, 28, 28))
-        if self.data_test:
-            return DataLoader(
-                dataset=self.data_test,
-                batch_size=self.batch_size_per_device,
-                num_workers=self.num_workers,
-                pin_memory=self.pin_memory,
-                shuffle=False,
-            )
-        else:
-            raise RuntimeError("The test dataset is not loaded.")
-
+    # def test_dataloader(self) -> DataLoader[tuple[torch.Tensor, torch.Tensor]]:
+    #     self.data_predict = GaussianDataset((self.predict_size, 1, 28, 28))
+    #     if self.data_test:
+    #         return DataLoader(
+    #             dataset=self.data_test,
+    #             batch_size=self.batch_size_per_device,
+    #             num_workers=self.num_workers,
+    #             pin_memory=self.pin_memory,
+    #             shuffle=False,
+    #         )
+    #     else:
+    #         raise RuntimeError("The test dataset is not loaded.")
+    #
     def predict_dataloader(self) -> DataLoader[torch.Tensor]:
         if self.data_predict:
             return DataLoader(
@@ -146,9 +148,7 @@ if __name__ == "__main__":
 
     train_loader = mnist.train_dataloader()
     val_loader = mnist.val_dataloader()
-    test_loader = mnist.test_dataloader()
 
-    print(len(train_loader.dataset), len(val_loader.dataset), len(test_loader.dataset))
+    print(len(train_loader.dataset), len(val_loader.dataset))
     print(train_loader.dataset[0][0].shape)
     print(val_loader.dataset[0][0].shape)
-    print(test_loader.dataset[0][0].shape)

src/diffusion/losses.py
@@ -1,13 +1,18 @@
-import math
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+import numpy as np
 import torch
 from torch.nn.functional import mse_loss
 
 from diffusion.diffusion_factors import Factors
 from diffusion.diffusion_terms import DiffusionTerms
 from diffusion.gaussian_utils import discretized_gaussian_log_likelihood, gaussian_kl
+
+
+def mean_flat(tensor: torch.Tensor) -> torch.Tensor:
+    """Compute the mean of a tensor while flattening it."""
+    return tensor.mean(dim=list(range(1, len(tensor.shape))))
 
 
 @dataclass
@@ -74,14 +79,16 @@ class MseMeanEpsilonSimple(DiffusionLoss):
 
 class VLB(DiffusionLoss):
     def forward(self, inputs: LossInputs) -> torch.Tensor:
-        loss = gaussian_kl(inputs.target.mean, inputs.target.log_variance, inputs.predicted.mean, inputs.predicted.log_variance)
-        decoder_nnl = -discretized_gaussian_log_likelihood(inputs.predicted.x_start, inputs.predicted.mean, inputs.predicted.log_variance)
+        kl_loss = gaussian_kl(inputs.target.mean, inputs.target.log_variance, inputs.predicted.mean, inputs.predicted.log_variance)
+        decoder_nnl = -discretized_gaussian_log_likelihood(inputs.target.x_start, inputs.predicted.mean, inputs.predicted.log_variance)
+
+        kl_loss = mean_flat(kl_loss) / np.log(2.0)
+        decoder_nnl = mean_flat(decoder_nnl) / np.log(2.0)
 
         idx = torch.where(inputs.timesteps == 0)
-        loss[idx] = decoder_nnl[idx]
-        loss = loss.mean() / math.sqrt(2.0)
+        kl_loss[idx] = decoder_nnl[idx]
 
-        return loss
+        return kl_loss.mean()
 
 
 class Hybrid(DiffusionLoss):

src/diffusion/schedulers.py
@@ -10,8 +10,9 @@ class Scheduler(Protocol):
 class LinearScheduler:
     def __init__(self, timesteps: int, start: float, end: float) -> None:
         self.timesteps = timesteps
-        self.start = start
-        self.end = end
+        self.scale = 1000 / timesteps
+        self.start = start * self.scale
+        self.end = end * self.scale
 
     def schedule(self) -> torch.Tensor:
         return torch.linspace(self.start, self.end, self.timesteps)

src/diffusion/scripts/fid.py
@@ -1,7 +1,5 @@
-from pathlib import Path
-
+import torch
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, TensorDataset
 from torchmetrics.image.fid import FrechetInceptionDistance
 from tqdm import tqdm
 
@@ -49,9 +47,8 @@ transform = transforms.Compose(
 
 import hydra
 import rootutils
-import torch
 from lightning import LightningDataModule
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 
 from diffusion.utils.extras import extras
 from diffusion.utils.ranked_logger import RankedLogger
@@ -64,63 +61,71 @@ configs_path = root_path / "configs"
 
 log = RankedLogger(__name__, rank_zero_only=True)
 
-device = "cuda:0"
+device = "cpu"  # "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
-def fid(sample_config: DictConfig):
-    run_path = Path(sample_config.paths.output_dir)
-
-    cfg = OmegaConf.load(run_path / "config.yaml")
-    cfg.data.batch_size = sample_config.batch_size
-    # cfg.data.predict_samples = sample_config.predict_samples
-
+def fid(cfg: DictConfig):
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
-    print(datamodule)
-
     datamodule.prepare_data()
     datamodule.setup("fit")
+    print(datamodule)
+
+    # datamodule.prepare_data()
+    # datamodule.setup("fit")
     train_loader = datamodule.train_dataloader()
-    test_loader = datamodule.test_dataloader()
-    if (run_path / "fid.pt").exists():
-        log.info(f"Loading FID from {run_path / 'fid.pt'}")
-        fid = torch.load(run_path / "fid.pt")
-    else:
-        log.info(f"Instantiating datamodule <{cfg.data._target_}>")
-        fid = FrechetInceptionDistance(feature=2048).to(device)
-        total = 0
-        for batch in tqdm(train_loader):
-            x, _ = batch
-            total += len(x)
-            x = x.to(device)
-            x = ((x + 1) * 127.5).clamp(0, 255).to(torch.uint8)
-            x = x.expand(-1, 3, -1, -1)
-            fid.update(x, real=True)
-            # if total > 5000:
-            #     break
+    test_loader = datamodule.val_dataloader()
 
-        torch.save(fid, run_path / "fid.pt")
+    fid = FrechetInceptionDistance(feature=2048, reset_real_features=False, normalize=False).to(device)
 
-        print(f"Total real: {total}")
-    images = torch.load(run_path / "predictions/result.pt")
-    dataset = TensorDataset(images)
-    dataloader = DataLoader(dataset, batch_size=sample_config.batch_size, shuffle=False)
-    # test_loader = dataloader
-    total = 0
-    for batch in tqdm(test_loader):
-        # x, _ = batch
-        x = batch[0]
-        total += len(x)
-        x = x.to(device)
+    for batch in tqdm(train_loader):
+        x, _ = batch
         x = ((x + 1) * 127.5).clamp(0, 255).to(torch.uint8)
-        x = x.expand(-1, 3, -1, -1)
-        fid.update(x, real=False)
-        if total > 1000:
-            break
+        print(x.shape)
+        print(x.min(), x.max())
 
-    print("Computing FID...")
-    print(f"Total fake: {total}")
-    print(fid.compute())
+        fid.update(x, real=True)
+        break
 
+    # if (run_path / "fid.pt").exists():
+    #     log.info(f"Loading FID from {run_path / 'fid.pt'}")
+    #     fid = torch.load(run_path / "fid.pt")
+    # else:
+    #     log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    #     fid = FrechetInceptionDistance(feature=2048).to(device)
+    #     total = 0
+    #     for batch in tqdm(train_loader):
+    #         x, _ = batch
+    #         total += len(x)
+    #         x = x.to(device)
+    #         x = ((x + 1) * 127.5).clamp(0, 255).to(torch.uint8)
+    #         x = x.expand(-1, 3, -1, -1)
+    #         fid.update(x, real=True)
+    #         # if total > 5000:
+    #         #     break
+    #
+    #     torch.save(fid, run_path / "fid.pt")
+    #
+    #     print(f"Total real: {total}")
+    # images = torch.load(run_path / "predictions/result.pt")
+    # dataset = TensorDataset(images)
+    # dataloader = DataLoader(dataset, batch_size=sample_config.batch_size, shuffle=False)
+    # # test_loader = dataloader
+    # total = 0
+    # for batch in tqdm(test_loader):
+    #     # x, _ = batch
+    #     x = batch[0]
+    #     total += len(x)
+    #     x = x.to(device)
+    #     x = ((x + 1) * 127.5).clamp(0, 255).to(torch.uint8)
+    #     x = x.expand(-1, 3, -1, -1)
+    #     fid.update(x, real=False)
+    #     if total > 1000:
+    #         break
+    #
+    # print("Computing FID...")
+    # print(f"Total fake: {total}")
+    # print(fid.compute())
+    #
     #     torch.save(fid, run_path / "fid.pt")
     #
     # images = torch.load(run_path / "predictions/result.pt")
@@ -138,7 +143,7 @@ def fid(sample_config: DictConfig):
     # print(score)
 
 
-@custom_main(version_base="1.3", config_path=str(configs_path), config_name="sample.yaml")
+@custom_main(version_base="1.3", config_path=str(configs_path), config_name="train.yaml")
 def main(cfg: DictConfig) -> float | None:
     extras(cfg)
     fid(cfg)